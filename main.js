f2758ab08bf9b22fb2c053b78356bc119421b54f
error042 <philipp.magel@web4train.de>
2021-11-15 00:48:49 +0100

    Commit with the animated blob

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+
 const btn = document.querySelector(".fancy-burger");
 
 btn.addEventListener('click', () => {
     btn.querySelectorAll('span').forEach((span) => span.classList.toggle('open'))
 })
+
+const tween = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob1'},
+    { path: '#blob2'},
+    { repeat: 999, duration: 3000, yoyo: true}
+)
+    tween.start()
