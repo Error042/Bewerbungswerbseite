0bb6dfef88f2dbe8620495184717c0cea9155df6
Error042 <pmagel95@gmail.com>
2022-02-10 23:55:04 +0100

    First commit of new Layout
    -added intro content
    -added intro background

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-/*
-* In this file I created the Main menu.
-*/
-
-const btn = document.querySelector(".fancy-burger");
-
-btn.addEventListener('click', () => {
-    btn.querySelectorAll('span').forEach((span) => span.classList.toggle('open'))
-})
